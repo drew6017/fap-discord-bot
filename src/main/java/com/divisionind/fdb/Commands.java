29657d687d4b9e34bcc2d23ebe54b2bb431ade49
drew6017 <drew6017@gmail.com>
2019-05-23 05:32:53 -0500

    decided to add an additional space in the xp command

--- src/main/java/com/divisionind/fdb/Commands.java
+++ src/main/java/com/divisionind/fdb/Commands.java
@@ -466,7 +466,7 @@ public class Commands {
                     NumberFormat numFor = NumberFormat.getNumberInstance();
                     LevelSystem levelSystem = FapBot.getLevelSystem();
                     event.getChannel().sendFile(bao.toByteArray(), "faplevel.png", new MessageBuilder(
-                            String.format("**Server Level: ** %s\n**Server Xp:** %s / 960\n**Server Rank:** #%s\n**Gamer Level:** %s\n**Gamer Xp:** %s / 96,000\n**Gamer Rank:** #%s",
+                            String.format("**Server Level: ** %s\n**Server Xp:** %s / 960\n**Server Rank:** #%s\n\n**Gamer Level:** %s\n**Gamer Xp:** %s / 96,000\n**Gamer Rank:** #%s",
                                     server_level, server_xp, numFor.format(levelSystem.getRank(LevelSystem.Level.SERVER, discord_id)), gamer_level, numFor.format(game_xp), numFor.format(levelSystem.getRank(LevelSystem.Level.GAMER, discord_id))
                             )).build()).queue();
                 } else {
